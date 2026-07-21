79022e6db203d629c5a73456f47918864ea9984e
Sakib Hadziavdic <sakib@sake.ba>
2026-07-21 09:17:05 +0200

    fix: remove bare-name keying from dependency index to prevent collisions

--- modules/core/src/ba/sake/basamake/navigation/DependencySourceIndexing.scala
+++ modules/core/src/ba/sake/basamake/navigation/DependencySourceIndexing.scala
@@ -100,8 +100,7 @@ object DependencySourceIndexing extends StrictLogging {
       }
       val symbolDefinitions =
         definitions.groupMap(_.symbol)(d => new Location(sourceUri, d.range)) ++
-          definitions.groupMap(_.ownerName)(d => new Location(sourceUri, d.range)) ++
-          definitions.groupMap(_.name)(d => new Location(sourceUri, d.range))
+          definitions.groupMap(_.ownerName)(d => new Location(sourceUri, d.range))
       val documentSymbols =
         definitions.flatMap { defn =>
           val symbolInfo = new SymbolInformation()
