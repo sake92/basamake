dceb060b79af0ac867156fd0f901eba535069969
Sakib Hadziavdic <sakib@sake.ba>
2026-08-17 16:20:49 +0200

    .

--- examples/hello/sbt/src/main/scala/Main.scala
+++ examples/hello/sbt/src/main/scala/Main.scala
@@ -2,13 +2,15 @@ package basamake.sbt
 
 import upickle.default._
 
+import java.util.ArrayList
+
 @main def hello(): Unit =
   val ccc = Array(1, 2, 3)
   val d = ccc
   println("Hello world!")
   println(msg)
   write(Seq(1, 2, 3))
-  new java.lang.Object()
+  val x: ArrayList[Int] = new ArrayList()
  
 def msg = 
   sbtutils.getMsg()
